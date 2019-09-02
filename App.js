4fd7705391795b5283c9fd9cb75e1c16fee032c9
kuwahara <kuwahara.shohei@slogan.jp>
2019-09-03 01:35:35 +0900

    タブ遷移出来るようにした。

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import WelcomeScreen from './screens/WelcomeScreen'
+import HomeScreen from './screens/HomeScreen'
+import AddScreen from './screens/AddScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <WelcomeScreen />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    const NavigatorTab = createAppContainer(
+      createBottomTabNavigator({
+        welcome: { screen: WelcomeScreen },
+        main: createBottomTabNavigator({
+          homeStack: { screen: HomeScreen },
+          addStack: { screen: AddScreen },
+          profileStack: { screen: ProfileScreen }
+        })
+      })
+    );
+    return (
+      <View style={styles.container}>
+        <NavigatorTab />
+      </View>
+    );
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
